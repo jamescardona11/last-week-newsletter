c9f0fe4bc44ea292baca6c93f37400bb438bb3fe
jamescardona11 <65878592+jamescardona11@users.noreply.github.com>
2024-02-14 23:39:12 -0500

    Update sitemetada

--- src/site-metadata.ts
+++ src/site-metadata.ts
@@ -1,10 +1,10 @@
 const siteMetadata = {
   title: 'Last Week - Flutter',
   description: 'Discover the latest in Flutter and Dart week by week',
-  siteUrl: 'https://blogfolio-astro.vercel.app/',
+  siteUrl: 'https://last-week-newsletter.vercel.app/',
   language: 'en-us',
-  siteRepo: 'https://github.com/jamescardona11',
-  siteLogo: '/static/images/logo.png',
+  siteRepo: 'https://github.com/jamescardona11/last-week-newsletter',
+  siteLogo: '/static/logo.png',
   github: 'jamescardona11',
   linkedin: 'jamescardona11',
   twitter: 'jamescardona11_',
