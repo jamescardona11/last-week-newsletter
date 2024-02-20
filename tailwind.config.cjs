8555e6f91cce64f01810548386a5ba5e93a321e8
jamescardona11 <65878592+jamescardona11@users.noreply.github.com>
2024-02-20 10:34:46 -0500

    Remove switch theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,7 @@ const colors = require('tailwindcss/colors')
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-  darkMode: 'class',
+  darkMode: 'dark',
   theme: {
     container: {
       center: true,
